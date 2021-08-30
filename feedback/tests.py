fbf425032c2c04a6dccd711d94c017b15a28366f
“sakshamtandon” <saksham12356@gmail.com>
2021-08-30 18:10:13 +0530

    run python code to check list comprehension

--- feedback/tests.py
+++ feedback/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+#from django.test import TestCase
 
 # Create your tests here.
+jsn = [{"name":"Saksham","roll":171071}]
+# print(jsn)
+# print('type',type(jsn)
+for val in jsn:
+  name = val["name"]
+print(name)
